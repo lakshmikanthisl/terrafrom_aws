ba1b463ec514f03b09b2921366b452259513eb17
lakshmikanthisl <lakshmikanthisl@gmail.com>
2023-05-24 07:19:08 +0530

    FIrst commit

---
+++ Modules/VPC/variable.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {
+    description = "VPC CIDR range"
+    default = "10.2.0.0/16" 
+}
+
+variable "location" {
+    description = "Location of Resources"
+    default = "ap-south-1"
+}
